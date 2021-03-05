a5ddddf6ff99dc4633a1e55391adca05f57cb7c1
CaptainPlusPlus <55258946+CaptainPlusPlus@users.noreply.github.com>
2021-03-05 19:02:53 +0100

    Update AND_of_range.h

--- AND_of_range.h
+++ AND_of_range.h
@@ -14,16 +14,12 @@ int and_range(int l, int r)
   return (sequence_mask & (xor_msb ^ r_msb)) & r; /*Nullify mask if MSB is different in l and r*/
 }
 
-int msb(unsigned int v)
+int msb(register unsigned int x)
 {
-  static const int pos[32] = {0, 1, 28, 2, 29, 14, 24, 3,
-    30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19,
-    16, 7, 26, 12, 18, 6, 11, 5, 10, 9};
-  v |= v >> 1;
-  v |= v >> 2;
-  v |= v >> 4;
-  v |= v >> 8;
-  v |= v >> 16;
-  v = (v >> 1) + 1;
-  return pos[(v * 0x077CB531UL) >> 27];
+        x |= (x >> 1);
+        x |= (x >> 2);
+        x |= (x >> 4);
+        x |= (x >> 8);
+        x |= (x >> 16);
+        return(x & ~(x >> 1));
 }
