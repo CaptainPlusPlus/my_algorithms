445f2e64d7e24a9d4c5caad167471e3756d1ace1
CaptainPlusPlus <55258946+CaptainPlusPlus@users.noreply.github.com>
2023-11-19 13:37:12 +0100

    Update is_upper.h

--- is_upper.h
+++ is_upper.h
@@ -1,8 +1,8 @@
 /************************************** IS_UPPER ***************************************/
 /* This tiny macro checks whether a character is uppercase */
-/* It uses NO CONDITIONAL STATEMENTS, hence it is extremely powerful in systems where read ahead is crucial */
+/* It uses NO CONDITIONAL STATEMENTS, hence it is useful in systems where read ahead is crucial */
 /* The usage is limited for systems supporting the old ASCII 32 non extended standart, and older versions of unicode */
-/* It can be modified to suit different standarts. */
+/* Can be modified to suit different standarts. */
 /***************************************************************************************/
 
 #define IS_UPPER(input) !!(((input - 1) % 90UL) >> 6)
